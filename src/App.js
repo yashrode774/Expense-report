1606f5f864af3c18b2e1f3474b9247197ece4060
Yash Rode <yashrode774@gmail.com>
2024-01-23 19:21:10 +0530

    initial app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpensesList from "./Components/ExpensesList";
+
+function App() {
+  return (
+    <ExpensesList/>
+  );
+}
+
+export default App;
---
+++ src/Components/ExpensesList.js
@@ -0,0 +1,10 @@
+const ExpensesList = () => {
+    return (
+        <ul id="expenses-list">
+            <li>lunch</li>
+            <li>dinner</li>
+        </ul>
+    );
+}
+
+export default ExpensesList;
